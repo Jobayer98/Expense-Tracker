f1ddc012fbd9acf343ccd5e778dd752ddda60e51
Jobayer98 <jobayerrahman0220@gmail.com>
2023-06-22 01:29:04 +0600

    add Income, Tracker and Expense components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import Expense from "./components/Expense/Expense";
+import Tracker from "./components/ExpenseTracker/Tracker";
+import Income from "./components/Income/Income";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Income />
+      <Tracker />
+      <Expense />
+    </div>
+  );
 };
 
 export default App;
