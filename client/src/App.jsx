301d551f4d0e1c70564a9fb110e0bdf8e2e8edff
Jobayer98 <jobayerrahman0220@gmail.com>
2023-06-22 01:43:34 +0600

    move all the components in center

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,29 @@
+//External module
+import Grid from "@mui/material/Grid";
+// Internal module
 import Expense from "./components/Expense/Expense";
 import Tracker from "./components/ExpenseTracker/Tracker";
 import Income from "./components/Income/Income";
 
 const App = () => {
   return (
-    <div>
-      <Income />
-      <Tracker />
-      <Expense />
-    </div>
+    <Grid
+      container
+      spacing={2}
+      justifyContent={"center"}
+      alignItems={"center"}
+      style={{ minHeight: "100vh" }}
+    >
+      <Grid item xs={12} sm={4} md={3}>
+        <Income />
+      </Grid>
+      <Grid item xs={12} sm={4} md={3}>
+        <Tracker />
+      </Grid>
+      <Grid item xs={12} sm={4} md={3}>
+        <Expense />
+      </Grid>
+    </Grid>
   );
 };
 
